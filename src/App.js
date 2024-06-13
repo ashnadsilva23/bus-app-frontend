d438d8b4ad511c5c870c4288b672621af90614d3
ashnadsilva23 <ashnadsilva2309@gmail.com>
2024-06-14 02:21:28 +0530

    project completed

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import AddUser from './components/AddUser';
+import LoginUser from './components/LoginUser';
 
 function App() {
   return (
     <div>
       <BrowserRouter>
       <Routes>
+      <Route path='/' element={<LoginUser/>}/>
+
         <Route path='/reg' element={<AddUser/>}/>
       </Routes>
       </BrowserRouter>
