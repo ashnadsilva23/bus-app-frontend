d721e3cddc6ccfa88ea845dd1bf2054c4c33ab2f
ashnadsilva23 <ashnadsilva2309@gmail.com>
2024-06-14 10:25:10 +0530

    project completed

--- src/components/LoginUser.jsx
+++ src/components/LoginUser.jsx
@@ -1,79 +1,70 @@
 import React, { useState } from 'react'
 import axios from 'axios'
-import { Link } from 'react-router-dom'
-import Navbar from './Navbar'
-
-const Login = () => {
-
-    const [data, changeData] = useState([
-
-        {
-           
-            "email": "",
-            "password": ""
-
-        }
-
-    ])
+import { Link, useNavigate } from 'react-router-dom'
 
 
 
-    const inputHandler = (event) => {
-        changeData({ ...data, [event.target.name]: event.target.value })
-
-
+const LoginUser = () => {
+    const [data, setdata] = useState({
+        "email": "",
+        "password": ""
+    })
+    const inputhandler = (event) => {
+        setdata({ ...data, [event.target.name]: event.target.value })
     }
-    
     const readValue = () => {
-        axios.post("http://localhost:8080/login",data).then(
-            (response)=>{
-                console.log(response.data)
-                if (response.data.status=="success") {                 
-                    alert("login success")
-                    
-                } else {
-                    alert("failed to login")
-                    
+        axios.post("http://localhost:8080/login", data).then(
+            (response) => {
+                if (response.data.status == "success") {
+                    sessionStorage.setItem("token", response.data.token)
+                    sessionStorage.setItem("userid", response.data.userid)
+                    navigate("/add")
+                }
+                else {
+                    alert(response.data.status)
                 }
             }
-        ).catch().finally()
+        ).catch(
+            (error) => {
+                console.log(error.message)
+                alert(error.message)
+            }
+        ).finally()
     }
+    let navigate = useNavigate()
     return (
         <div>
-            <Navbar/>
-
-            <h1 align="center"><u>Login</u></h1>
+             
+            <h1 align="center"><u>Login </u></h1><br></br>
+            
             <div className="container">
-                <div className="card" >
-
-                    <div class="card-body">
-                        <div className="row">
+            <div className="card">
+            <div class="card-body">
+                <div className="row">
+                    <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                        <div className="row g-3">
                             <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
-                                <div className="row g-3">
-                                <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
-                                <label htmlFor="" className="form-label">Email</label><br></br>
-                                        <input type="text" className="form-control" name='email' value={data.userName} onChange={inputHandler} />
-                                    </div>
-                                    <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
-                                    <label htmlFor="" className="form-label">Password</label>
-                                        <input type="password" className="form-control" name='password' value={data.passward} onChange={inputHandler} />
-                                    </div>
-                                  
-
-                                    <div className="col col-12 col-sm-6 col-md-6 col lg-6 col-xl-6 col-xxl-6">
-                                        <button className="btn btn-success" onClick={readValue}>Login</button><br></br><br></br>
-
-                                        <p>new users click to register <Link to="/reg">sign up</Link></p>
-                                                                    
-                                          </div>
-                                </div>
+                                <label htmlFor="" className="form-label"><b>Email</b></label>
+                                <input type="text" className="form-control" name='email' value={data.email} onChange={inputhandler} />
+                            </div>
+                            <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                                <label htmlFor="" className="form-label"><b>password</b></label>
+                                <input type="password" name="password" value={data.password} onChange={inputhandler} id="" className="form-control" />
+                            </div>
+                            <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                                <button className="btn btn-success" onClick={readValue}>LOGIN</button>
+                            </div>
+                            <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                                <p>  new users click to Signup <Link to="/reg">Sign Up</Link></p>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
+        </div>
+        </div>
     )
 }
 
-export default Login
+export default LoginUser
